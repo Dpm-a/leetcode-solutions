af3659005c23cb1f32caa6bedb47627d464893d5
Dpm-a <d.innocenti6@studenti.unipi.it>
2024-07-27 17:28:38 +0200

    init SQL folder & first exercise

---
+++ SQL/1164_Product_Price_at_a_Given_Date.sql
@@ -0,0 +1,18 @@
+/*
+SOLUTION & EXPLANATION:
+https://leetcode.com/problems/product-price-at-a-given-date/
+*/
+
+with valid_product_ids as (
+    select product_id, max(change_date) as max_change_date
+    from products
+    where change_date <= "2019-08-16"
+    group by product_id
+)
+
+select p.product_id, p.new_price as price
+from valid_product_ids as v join Products p on p.product_id = t.product_id and p.change_date = t.max_change_date
+union
+select product_id, 10
+from Products
+where product_id not in (select product_id from valid_product_ids)
