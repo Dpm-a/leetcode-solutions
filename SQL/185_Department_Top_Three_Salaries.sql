2876210d1ca7c21a655a4222d23450c7675decf7
Dpm-a <d.innocenti6@studenti.unipi.it>
2024-07-28 16:56:41 +0200

    upload 3th and 4th

---
+++ SQL/185_Department_Top_Three_Salaries.sql
@@ -0,0 +1,16 @@
+/*
+SOLUTION & EXPLANATION:
+https://leetcode.com/problems/department-top-three-salaries/solutions/5549154/easy-solution-dense-rank-over
+*/
+
+with top3ByDept as (
+    select d.name as Department, 
+        e.name as Employee, 
+        salary,
+        dense_rank() over(partition by d.name order by salary desc) as rankByDept
+    from Department d join Employee e on e.departmentId = d.id
+)
+
+select distinct Department, Employee, salary 
+from top3ByDept 
+where rankByDept <= 3
